e07099222dfd81f7f57002f75afaaa01faa44f6e
Sean Claflin <seanclaflin@protonmail.com>
2023-03-21 21:59:39 -0500

    streamlined creation of webUI config file

--- index.js
+++ index.js
@@ -7,15 +7,10 @@ const config = parse((await fs.readFile('./config.yml')).toString());
 const nvr = await FrugalNVR.fromObject(config);
 
 try {
-	// compile the webUI config
-	const webUI = {
-		...constants.WEB_UI_CONFIG,
-		...config?.webUI
-	};
 	// write a config file to the compiled web site so it can connect to the
 	// configured hostname & ports.
 	await fs.writeFile('./web/config.json', JSON.stringify({
-		apiUrl: `http://${config.hostname}:${webUI.port}`,
+		apiUrl: `http://${config.hostname}:${config.webUI.port || constants.WEB_UI_DEFAULT_PORT}`,
 		streamUrl: `http://${config.hostname}:${config.mediaServer.http.port}`
 	}));
 }
