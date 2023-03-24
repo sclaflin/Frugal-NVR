2e177fb6cd321a63b6a9d03d80ef95ec3d5b39cf
Sean Claflin <seanclaflin@protonmail.com>
2023-03-24 12:43:46 -0500

    Fixed/updated units of time & bytes

--- ui/lib/Utils.js
+++ ui/lib/Utils.js
@@ -2,10 +2,10 @@ export function durationString(duration) {
 	if (!Number.isInteger(duration))
 		throw new TypeError('duration must be an integer.');
 	const durationParts = [
-		{ value: Math.floor(duration / (24 * 60 * 60)), label: 'd' },
-		{ value: Math.floor((duration % (24 * 60 * 60)) / (60 * 60)), label: 'h' },
-		{ value: Math.floor(((duration % (24 * 60 * 60)) % (60 * 60)) / 60), label: 'm' },
-		{ value: Math.floor((((duration % (24 * 60 * 60)) % (60 * 60)) % 60)), label: 's' }
+		{ value: Math.floor(duration / (24 * 60 * 60)), label: 'D' },
+		{ value: Math.floor((duration % (24 * 60 * 60)) / (60 * 60)), label: 'H' },
+		{ value: Math.floor(((duration % (24 * 60 * 60)) % (60 * 60)) / 60), label: 'M' },
+		{ value: Math.floor((((duration % (24 * 60 * 60)) % (60 * 60)) % 60)), label: 'S' }
 	];
 	const index = durationParts.findIndex(v => v.value !== 0);
 	return durationParts.slice(index, index + 2).map(v => `${v.value}${v.label}`).join(' ');
@@ -22,7 +22,7 @@ export function bytesString(bytes, scale = 0) {
 	const tebibyte = 2 ** 40;
 	const byteScales = [
 		{ value: Math.round(bytes / tebibyte * 10 ** scale) / 10 ** scale, label: 'TiB' },
-		{ value: Math.round(bytes % tebibyte / gibibyte * 10 ** scale) / 10 ** scale, label: 'Gib' },
+		{ value: Math.round(bytes % tebibyte / gibibyte * 10 ** scale) / 10 ** scale, label: 'GiB' },
 		{ value: Math.round(bytes % tebibyte % gibibyte / mebibyte * 10 ** scale) / 10 ** scale, label: 'MiB' },
 		{ value: Math.round(bytes % tebibyte % gibibyte % mebibyte / kibibyte * 10 ** scale) / 10 ** scale, label: 'KiB' },
 		{ value: bytes % tebibyte % gibibyte % mebibyte % kibibyte, label: 'B' }
