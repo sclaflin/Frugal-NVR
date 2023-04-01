04bf470c5ea4216afca43b38a758df705f4fb7a4
Sean Claflin <seanclaflin@protonmail.com>
2023-04-01 14:34:44 -0500

    Overview now reflections camera motion status

--- ui/lib/Overview.js
+++ ui/lib/Overview.js
@@ -17,8 +17,20 @@ export default class Overview extends LitElement {
 				display: grid;
 			}
 			.composite > * {
+				position: relative;
+			}
+			.composite img {
 				width: 100%;
+				height: 100%;
 			} 
+			.motion-sensor {
+				animation: pulse 1s infinite;
+				position: absolute;
+				top: 1em;
+				left: 1em;
+				color: #ffffff;
+				width: 1.5em;
+			}
 			.composite.cam-grid-1 {
 				grid-template-columns: 1fr;
 			}
@@ -31,9 +43,23 @@ export default class Overview extends LitElement {
 			.composite.cam-grid-4 {
 				grid-template-columns: 1fr 1fr 1fr 1fr;
 			}
+			@keyframes pulse {
+				0% {
+					transform-scale(0.9);
+					box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
+				}
+				70% {
+					transform-scale(1);
+					box-shadow: 0 0 0 10px rgba(255, 255, 255, 0);
+				}
+				100% {
+					transform-scale(0.9);
+				}
+			}
 		`
 	];
 	#thumbHandler = () => this.requestUpdate();
+	#motionHandler = () => this.requestUpdate();
 	get cameras() {
 		return this.#cameras;
 	}
@@ -43,8 +69,12 @@ export default class Overview extends LitElement {
 
 		const lastCameras = this.cameras;
 		lastCameras?.off('thumb', this.#thumbHandler);
+		this.cameras?.off('motionStart', this.#motionHandler);
+		this.cameras?.off('motionStop', this.#motionHandler);
 		this.#cameras = v;
 		this.cameras.on('thumb', this.#thumbHandler);
+		this.cameras.on('motionStart', this.#motionHandler);
+		this.cameras.on('motionStop', this.#motionHandler);
 	}
 	connectedCallback() {
 		super.connectedCallback();
@@ -113,7 +143,13 @@ export default class Overview extends LitElement {
 			if (col === cols && row === 1) classes.push('rounded-top-right');
 			if (col === 1 && row === rows) classes.push('rounded-bottom-left');
 			if (col === cols && row === rows) classes.push('rounded-bottom-right');
-			return html`<img class="clickable ${classes.join(' ')}" @click=${() => this.thumbClickHandler(camera)} src=${camera.thumb}/>`;
+			// if (camera.activeMotion) classes.push('')
+			return html`
+				<div>
+					<img class="clickable ${classes.join(' ')}" @click=${() => this.thumbClickHandler(camera)} src=${camera.thumb}/>
+					${camera.activeMotion ? html`<svg class="motion-sensor fade-in dark-bg border rounded padded" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M10 .2C9 .2 8.2 1 8.2 2S9 3.8 10 3.8 11.8 3 11.8 2 11 .2 10 .2m5.67.8A7.33 7.33 0 0 0 23 8.33V7a6 6 0 0 1-6-6h-1.33m2.66 0c0 2.58 2.09 4.67 4.67 4.67V4.33c-1.84 0-3.33-1.49-3.33-3.33h-1.34M21 1a2 2 0 0 0 2 2V1h-2M7.92 4.03c-.17 0-.34.03-.5.08L2 5.8V11h1.8V7.33l2.11-.66L2 22h1.8l2.87-8.11L9 17v5h1.8v-6.41l-2.49-4.54.73-2.87L10.12 10H15V8.2h-3.62l-2-3.33c-.3-.5-.84-.84-1.46-.84Z"/></svg>` : ''}
+				</div>
+			`;
 		});
 
 		return html`
